1ef69dd7cc8b681593b585ae0e6d6d8570000406
Hang Wang <hangwan@microsoft.com>
2022-08-31 17:22:20 +0800

    Update petclinic.tf

--- petclinic/petclinic.tf
+++ petclinic/petclinic.tf
@@ -3,12 +3,12 @@ provider "azurerm" {
 }
 
 resource "azurerm_resource_group" "example" {
-  name     = "petclinic-terraform"
+  name     = "petclinic_terraform"
   location = "East US"
 }
 
 resource "azurerm_spring_cloud_service" "example" {
-  name                = "petclinic-terraform-example-springcloud"
+  name                = "petclinic_terraform_example_springapps"
   resource_group_name = azurerm_resource_group.example.name
   location            = azurerm_resource_group.example.location
 
@@ -20,7 +20,7 @@ resource "azurerm_spring_cloud_service" "example" {
 }
 
 resource "azurerm_spring_cloud_app" "example" {
-  name                = "petclinic-terraform-example-springcloudapp"
+  name                = "petclinic_terraform_example_springapps"
   resource_group_name = azurerm_resource_group.example.name
   service_name        = azurerm_spring_cloud_service.example.name
   is_public           = true
